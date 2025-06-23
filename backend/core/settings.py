6a5761eaff619302d5f039817f739e083716ef1a
krish05s <bhuvakrish75@gmail.com>
2025-06-23 13:26:15 +0530

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -49,7 +49,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "https://portfolio-builder-coral.vercel.app",
 ]
 
 ALLOWED_HOSTS = ['*']  # Or use Render URL
